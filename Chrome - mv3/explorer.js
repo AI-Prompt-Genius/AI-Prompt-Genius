7aa6f68838344752f63bcdbc9733bf80e01ad900
Ben <benfinch04@gmail.com>
2022-12-13 18:53:37 -0700

    changed it so the icons are a grid to keep rows somewhat short. Also added version 1.02 to manifest.

--- Chrome - mv3/explorer.js
+++ Chrome - mv3/explorer.js
@@ -6,7 +6,7 @@ let main = document.querySelector(".main")
 function sliceString(str, num) { //created by ChatGPT
     // Check if the string is longer than num characters
     if (str.length > num) {
-        return `${str.slice(0, num)}...`;
+        return `${str.slice(0, num)}...`.replace(`<p>`, "").replace(`</p>`, "");
     }
     // If the string is not longer than num characters, return it as is
     return str;
