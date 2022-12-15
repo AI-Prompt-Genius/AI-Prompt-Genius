e360a359f679b9aa354e349d226b49c0578e9c1c
Ben <benfinch04@gmail.com>
2022-12-15 16:56:16 -0700

    updated build scripts to create a releases directory with zips of dist directories for easy uploading to firefox/chrome

--- build.bat
+++ build.bat
@@ -12,5 +12,9 @@ REM Copy the manifest files to their respective dist directories
 ROBOCOPY  "manifests\mv3-manifest" "dist_mv3" /xx
 ROBOCOPY  "manifests\mv2-manifest" "dist_mv2" /xx
 
+rem Zip the dist directories for releases to Chrome Web Store & firefox addons
+"%ProgramFiles(x86)%\7-Zip\7z.exe" a -tzip releases\dist_mv3.zip dist_mv3
+"%ProgramFiles(x86)%\7-Zip\7z.exe" a -tzip releases\dist_mv2.zip dist_mv2
+
 REM For debugging so the window doesn't close
-pause 
+pause
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # First, create the dist directories if they don't already exist
-mkdir -p dist_mv3 dist_mv2
+mkdir -p dist_mv3 dist_mv2 releases
 
 # Copy the files from "src" to "dist_mv3" and "dist_mv2"
 cp -r src/* dist_mv3
@@ -10,3 +10,7 @@ cp -r src/* dist_mv2
 # Copy the manifest files to their respective dist directories
 cp manifests/mv3-manifest/manifest.json dist_mv3
 cp manifests/mv2-manifest/manifest.json dist_mv2
+
+# Zip the dist directories
+zip -rj releases/dist_mv3.zip dist_mv3
+zip -rj releases/dist_mv2.zip dist_mv2
