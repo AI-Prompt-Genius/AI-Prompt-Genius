23e4a65e2619e2cc4bc4f473c1f52c69057678e4
Ben <benfinch04@gmail.com>
2022-12-17 19:52:58 -0700

    fixes old code formatting

--- src/thread.js
+++ src/thread.js
@@ -22,6 +22,7 @@ browser.storage.local.get(['threads']).then((result) => {
 function load_thread(c){
     for (let i = 0; i < c.length; i++) {
         let human = i % 2 === 0;
+        let bar = `<div class="flex items-center relative text-gray-200 bg-gray-800 px-4 py-2 text-xs font-sans"><button class="flex ml-auto gap-2"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>Copy code</button></div>`
         if (human) {
             var temp = h_template.content.cloneNode(true);
             temp.querySelector(".text").innerHTML = `<p>${c[i]}</p>`
@@ -29,7 +30,9 @@ function load_thread(c){
         }
         else{
             var temp = b_template.content.cloneNode(true);
-            temp.querySelector(".text").innerHTML = c[i]
+            let clipboard = `<i class="fa-regular clipboard fa-clipboard"></i>`
+            let copy_bar = `<div class="p-2 copy float-right">${clipboard} &nbsp; Copy code</div>`
+            temp.querySelector(".text").innerHTML = c[i].replace(bar, copy_bar).replace(`p-4`, "") // fixes formatting for weird code divs
             main.appendChild(temp)
         }
     }
