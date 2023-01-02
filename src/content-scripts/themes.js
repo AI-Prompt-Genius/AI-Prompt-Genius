f6d8cdd3a428985860aa5ca16be8b0feb567c3c5
aivct <eventloop@tutanota.com>
2023-01-02 00:45:16 -0500

    added ability to inject themes

--- src/content-scripts/themes.js
+++ src/content-scripts/themes.js
@@ -1,1 +1,18 @@
 console.log(`Loading themes...`);
+
+// the way that themes work is to inject it after everything else.
+// remember to expose themes in web_accessible_resources
+// inject theme 
+function injectStyle(file)
+{	
+	let head = document.querySelector('head');
+    let stylesheet = document.createElement('link');
+    stylesheet.setAttribute('rel', 'stylesheet');
+    stylesheet.setAttribute('type', 'text/css');
+    stylesheet.setAttribute('href', file);
+    head.appendChild(stylesheet);
+}
+
+injectStyle(browser.runtime.getURL('themes/novel.css'));
+
+	
