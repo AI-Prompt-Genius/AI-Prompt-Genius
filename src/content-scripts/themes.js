876e782159c60327db89c70da1c0eb0f5c16bdbd
aivct <eventloop@tutanota.com>
2023-01-03 10:28:10 -0500

    added theme selector button

--- src/content-scripts/themes.js
+++ src/content-scripts/themes.js
@@ -3,10 +3,13 @@ console.log(`Loading themes...`);
 // the way that themes work is to inject it after everything else.
 // remember to expose themes in web_accessible_resources
 // inject theme 
+var themeStylesheet;
+
 function injectStyle(file)
 {	
 	let head = document.querySelector('head');
     let stylesheet = document.createElement('link');
+	themeStylesheet = stylesheet;
     stylesheet.setAttribute('rel', 'stylesheet');
     stylesheet.setAttribute('type', 'text/css');
     stylesheet.setAttribute('href', file);
@@ -15,4 +18,46 @@ function injectStyle(file)
 
 injectStyle(browser.runtime.getURL('themes/cozy-fireplace.css'));
 
+// theme selector 
+function addThemeSelectButton()
+{
+	let icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em" style="fill: white" stroke="currentColor" ><path d="M512 256c0 .9 0 1.8 0 2.7c-.4 36.5-33.6 61.3-70.1 61.3H344c-26.5 0-48 21.5-48 48c0 3.4 .4 6.7 1 9.9c2.1 10.2 6.5 20 10.8 29.9c6.1 13.8 12.1 27.5 12.1 42c0 31.8-21.6 60.7-53.4 62c-3.5 .1-7 .2-10.6 .2C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256zM128 288c0-17.7-14.3-32-32-32s-32 14.3-32 32s14.3 32 32 32s32-14.3 32-32zm0-96c17.7 0 32-14.3 32-32s-14.3-32-32-32s-32 14.3-32 32s14.3 32 32 32zM288 96c0-17.7-14.3-32-32-32s-32 14.3-32 32s14.3 32 32 32s32-14.3 32-32zm96 96c17.7 0 32-14.3 32-32s-14.3-32-32-32s-32 14.3-32 32s14.3 32 32 32z"/></svg>`;
 	
+	let wrapper = document.createElement("a");
+	wrapper.id = "theme-select-button";
+	wrapper.setAttribute("class", 'flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm');
+	wrapper.innerHTML = `${icon}`;
+	
+	let themeSelect = document.createElement("select");
+	themeSelect.style.background = "transparent";
+	themeSelect.style.height = "19px";
+	themeSelect.style.width = "100%";
+	themeSelect.style.padding = "0";
+	themeSelect.style.color = "inherit";
+	themeSelect.style.fontFamily = "inherit";
+	themeSelect.style.fontSize = "inherit";
+	themeSelect.style.overflow = "visible";
+	themeSelect.style.border = "0";
+	
+	
+	let noThemeOption = document.createElement("option");
+	noThemeOption.value = "";
+	noThemeOption.innerHTML = "No Theme";
+	themeSelect.appendChild(noThemeOption);
+	
+	let themesList = ["cozy-fireplace.css"];
+	for(index = 0; index < themesList.length; index++)
+	{
+		let themeOption = document.createElement("option");
+		themeOption.value = themesList[index];
+		themeOption.innerHTML = themesList[index];
+		themeSelect.appendChild(themeOption);
+	}
+	
+	wrapper.appendChild(themeSelect);
+	
+	var nav = document.querySelector("nav");
+	nav.appendChild(wrapper);
+}
+
+addThemeSelectButton();
