b828c6e7393aa20be010f7ff7c58c54cd5d6c780
Ben Finch <aipromptgenius@gmail.com>
2023-03-08 22:18:08 -0700

    new ad

--- update_ad.sh
+++ update_ad.sh
@@ -5,7 +5,7 @@ git config --global user.email "aipromptgenius@gmail.com"
 git config --global user.name "Ben Finch"
 
 # Rest of the script
-echo '<em>Sponsored by the <u><a href="https://chrome.google.com/webstore/detail/chatgpt-by-openai/nijlhkhkkhnhidoondiobjdgommomifm?hl=en" target="_blank">ChatGPT Chrome Extension</a></u> - Access ChatGPT in all of your searches</em>' > public/ads/current.txt
+echo 'Sponsored by the <u><a href="https://chrome.google.com/webstore/detail/chatgpt-by-openai/nijlhkhkkhnhidoondiobjdgommomifm?hl=en" target="_blank">ChatGPT Chrome Extension</a></u> - Access ChatGPT in all of your searches. <em><a href="https://7yq2.short.gy/OwDoLv" target="_blank"><u>Advertise here.</u></a></em>' > public/ads/current.txt
 git add public/ads/current.txt
 git commit -m "Update ad"
 git push origin master
