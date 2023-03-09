072c4403d83d8247b251d70bdbb551868aa34e13
Ben <benfinch04@gmail.com>
2023-03-08 21:20:56 -0700

    rename

--- update_ad.sh
+++ update_ad.sh
@@ -5,7 +5,7 @@ git config --global user.email "${GIT_USER_EMAIL}"
 git config --global user.name "${GIT_USER_NAME}"
 
 # Rest of the script
-echo "new ad 1" > public/ads/testing.txt
+echo "new ad" > public/ads/testing.txt
 git add public/ads/testing.txt
 git commit -m "Update ad"
-git push origin ${{ github.ref }}
+git push origin master
