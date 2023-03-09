0fa75502e861db8a5af65cce1885d878c8966de7
Ben Finch <aipromptgenius@gmail.com>
2023-03-08 21:27:18 -0700

    rename

--- update_ad.sh
+++ update_ad.sh
@@ -5,7 +5,7 @@ git config --global user.email "aipromptgenius@gmail.com"
 git config --global user.name "Ben Finch"
 
 # Rest of the script
-echo "new ad" > public/ads/testing.txt
+echo "new ad testy testy test" > public/ads/testing.txt
 git add public/ads/testing.txt
 git commit -m "Update ad"
 git push origin master
