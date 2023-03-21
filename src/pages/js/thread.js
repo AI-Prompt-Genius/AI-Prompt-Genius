03ea95ac66b27439a16127361052607f503a6f74
Ben Finch <aipromptgenius@gmail.com>
2023-03-21 12:59:21 -0600

    fixed replacing left tags

--- src/pages/js/thread.js
+++ src/pages/js/thread.js
@@ -173,7 +173,7 @@ function load_branched_thread()
 					const copy_bar = `<div class="p-2 copy float-right">${clipboard} &nbsp; Copy code</div>`
 					const options = {backslashEscapesHTMLTags: true, tables: true, simplifiedAutoLink: true}
 					let converter = new showdown.Converter(options);
-					let markdown = fake_convo[i].replace(/```([\s\S]*?)```|<([^>]+)>/g, (match, codeBlock, lessThan) => { // replaces left tags not in code blocks, thanks ChatGPT
+					let markdown = fake_convo[i].replace(/```([\s\S]*?)```|<([^>]+)>/g, (match, codeBlock, lessThan) => { // replaces left tags < not in code blocks, thanks ChatGPT
 						if (codeBlock) {
 							return match; // leave code block as is
 						} else {
