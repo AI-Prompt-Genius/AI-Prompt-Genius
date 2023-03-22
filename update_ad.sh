a71b84df324c409cd733ccf3147a0b8128cc7d05
Ben Finch <aipromptgenius@gmail.com>
2023-03-22 16:39:05 -0600

    update ad schedule

--- update_ad.sh
+++ update_ad.sh
@@ -5,7 +5,9 @@ git config --global user.email "aipromptgenius@gmail.com"
 git config --global user.name "Ben Finch"
 
 # Rest of the script
-echo 'Sponsored by <u><a href="https://www.jaybird.ai/?utm_source=prompt-genius&utm_medium=banner&utm_campaign=march-2023" target="_blank">Jaybird.ai</a></u> - AI writing assistant designed specifically for developers.' > public/ads/current.txt
+echo 'Sponsored by <u><a href="https://www.usechatgpt.ai/install?ref=chatgptpromptgenius" target="_blank">UseChatGPT.AI</a></u> - Free ChatGPT Copilot on Chrome. Use ChatGPT (GPT-4 ✓) on any website without copy-pasting.' > public/ads/current.txt
+echo 'https://www.usechatgpt.ai/install?ref=chatgptpromptgenius' > public/ads/currentUrl.txt
 git add public/ads/current.txt
+git add public/ads/currentUrl.txt
 git commit -m "Update ad"
 git push origin master
