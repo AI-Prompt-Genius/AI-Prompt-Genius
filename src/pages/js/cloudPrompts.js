3187c08507e48d90617e48a5a1386fa52a56732b
Ben Finch <aipromptgenius@gmail.com>
2023-03-31 22:17:24 -0600

    cloud syncing is a mess

--- src/pages/js/cloudPrompts.js
+++ src/pages/js/cloudPrompts.js
@@ -22,6 +22,9 @@ async function alreadyLinked() {
     document.getElementById("unlinked-buttons").classList.add("d-none")
     document.getElementById("linkedDiv").classList.remove("d-none")
     document.getElementById("unlink").addEventListener("click", unlink)
+    chrome.storage.local.get({"prompts": []}, function (r){
+        syncPrompts([],[], r.prompts, r.prompts, result.sheetID)
+    })
 }
 
 async function unlink() {
@@ -92,12 +95,164 @@ function JSONtoNestedList(prompts) {
     values.push(headers)
     for (let prompt of prompts) {
         let list = []
-        for (let key of (Object.keys(prompt))) {
-            list.push(prompt[key])
+        for (let key of Object.keys(prompt)) {
+            if (Array.isArray(prompt[key])) {
+                list.push(prompt[key].join(";"));
+            } else {
+                list.push(prompt[key]);
+            }
         }
         values.push(list)
     }
     return values
+}
+
+async function updateSheetData(spreadsheetId, range, data) {
+    try {
+        const token = await getAuthToken();
+        const values = JSONtoNestedList(data);
+        const requestBody = {
+            values: values
+        };
+        const valueInputOption = "USER_ENTERED";
+        const endpointUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?valueInputOption=${valueInputOption}`;
+        const response = await fetch(endpointUrl, {
+            method: 'PUT',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
+        if (!response.ok) {
+            throw new Error('Failed to update spreadsheet');
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+function mergePrompts(localPrompts, cloudPrompts) {
+    // Create a copy of the local prompts array
+    const mergedPrompts = JSON.parse(JSON.stringify(localPrompts));
+
+    // Merge in cloud prompts
+    cloudPrompts.forEach(cloudPrompt => {
+        const existingIndex = mergedPrompts.findIndex(localPrompt => localPrompt.id === cloudPrompt.id);
+        if (existingIndex === -1) { // not an existing prompt
+            // Add new prompt
+            mergedPrompts.push(cloudPrompt);
+        } else {
+            // Update existing prompt
+            mergedPrompts[existingIndex] = Object.assign({}, mergedPrompts[existingIndex], cloudPrompt);
+        }
+    });
+
+    return mergedPrompts;
+}
+
+async function syncPrompts(deletedPrompts, newPrompts, changedPrompts, localPrompts, sheetId) {
+    try {
+        // Get prompts from the Google Sheets version
+        const sheetData = await getSheetData(sheetId, "Sheet1!A1:G");
+        console.log(sheetData)
+
+        // Remove deleted prompts from the cloud version
+        deletedPrompts.forEach(id => {
+            const index = sheetData.findIndex(prompt => prompt.id === id);
+            if (index !== -1) {
+                sheetData.splice(index, 1);
+            }
+        });
+
+        // Merge local and cloud version for changed prompts
+        changedPrompts.concat(newPrompts).forEach(changedP => {
+            let id = changedP.id
+            console.log(localPrompts)
+            const localPrompt = localPrompts.find(prompt => prompt.id === id);
+            const cloudPrompt = sheetData.find(prompt => prompt.id === id);
+
+            if (localPrompt) {
+                if (!cloudPrompt){
+                    sheetData.push(localPrompt)
+                }
+                else {
+                    // Merge the two prompts
+                    cloudPrompt.text = localPrompt?.text;
+                    cloudPrompt.time = localPrompt?.time;
+                    cloudPrompt.category = localPrompt?.category;
+                    console.log(localPrompt.tags)
+                    cloudPrompt.tags = localPrompt?.tags.join(";");
+
+                    // Find the index of the merged prompt in the sheetData array
+                    const index = sheetData.findIndex(prompt => prompt.id === id);
+
+                    // Replace the old prompt with the merged prompt
+                    if (index !== -1) {
+                        sheetData[index] = cloudPrompt;
+                    }
+                    else {
+                        sheetData.push(cloudPrompt);
+                    }
+                }
+            }
+        });
+
+        // Update the Chrome storage version with the merged data
+        const mergedData = mergePrompts(localPrompts, sheetData);
+        const correctTags = []
+        for (let prompt of mergedData){
+            console.log(prompt)
+            //prompt.tags = prompt.tags.split(";")
+            correctTags.push(prompt)
+        }
+        chrome.storage.local.set({'prompts': mergedData});
+
+        // Update the Google Sheets version with the merged data
+        await updateSheetData(sheetId, "Sheet1!A1:G", sheetData);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+async function getSheetData(spreadsheetId, range) {
+    try {
+        const mumboJumbo = "AIzaSyAjjnHsq4rkzK7jtjZ_zvs62lT8nqeQVoU" // this isn't dangerous but you can ignore it
+        const token = await getAuthToken();
+        const endpointUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${mumboJumbo}`;
+        const headers = new Headers();
+        headers.append("Authorization", `Bearer ${token}`);
+        const response = await fetch(endpointUrl, {
+            method: "GET",
+            headers: headers
+        });
+        if (!response.ok) {
+            throw new Error('Failed to fetch data from endpoint');
+        }
+        const data = await response.json();
+        console.log((data))
+        const headersRow = data.values[0];
+        const values = data.values.slice(1);
+        const jsonData = values.map(row => {
+            const obj = {};
+            headersRow.forEach((header, index) => {
+                if (header === "tags") {
+                    console.log("tags!")
+                    obj[header] = row[index].split(';');
+                }
+                else {
+                    obj[header] = row[index];
+                }
+            });
+            return obj;
+        });
+        console.log(jsonData)
+        return jsonData;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function checkForExisting(token) {
@@ -182,13 +337,3 @@ async function linkSheet() {
         console.error(error);
     }
 }
-
-async function l() {
-    try {
-        const token = await getAuthToken();
-        console.log(await checkForExisting(token));
-    } catch (error) {
-        console.error(error);
-    }
-}
-l();
