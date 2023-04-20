9d89fb71be7f6f3c1395769b9ae398d7d933f2dc
Ben Finch <aipromptgenius@gmail.com>
2023-04-19 23:30:15 -0600

    ad

--- update_ad.sh
+++ update_ad.sh
@@ -5,8 +5,8 @@ git config --global user.email "aipromptgenius@gmail.com"
 git config --global user.name "Ben Finch"
 
 # Rest of the script
-echo 'Sponsored by <u><a href="https://www.usechatgpt.ai/install?ref=chatgptpromptgenius" target="_blank">UseChatGPT.AI</a></u> - Free ChatGPT Copilot on Chrome. Use ChatGPT (GPT-4 ✓) on any website without copy-pasting.' > public/ads/current.txt
-echo 'https://www.usechatgpt.ai/install?ref=chatgptpromptgenius' > public/ads/currentUrl.txt
+echo 'Sponsored by <u><a href="https://imgcreator.zmo.ai/?ref=chatgptpromptgenius" target="_blank">ZMO.AI</a></u> - The Ultimate Free Generator for AI Anime, Photo, and Design. Text to image, image to image & ChatGPT powered AI designer.' > public/ads/current.txt
+echo 'https://imgcreator.zmo.ai/?ref=chatgptpromptgenius' > public/ads/currentUrl.txt
 git add public/ads/current.txt
 git add public/ads/currentUrl.txt
 git commit -m "Update ad"
