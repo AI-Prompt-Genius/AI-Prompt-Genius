f2c15d7b2d4e7bf32ebe4440b45dc4217e204f6c
Pixkk <30828435+pixkk@users.noreply.github.com>
2023-04-25 17:00:54 +0300

    Update localization.js

--- src/pages/js/localization.js
+++ src/pages/js/localization.js
@@ -76,6 +76,7 @@ async function translate(key, lang=language) {
                 // Return the translation
                 return json[key].message;
             }
+            return key + "- empty translation!";
         })
         .catch((error) => console.error(error));
 }
