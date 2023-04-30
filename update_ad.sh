3e03d8aa4e20ce08e6429ce672195a49c957ef63
Ben Finch <aipromptgenius@gmail.com>
2023-04-29 23:11:52 -0600

    update ad schedule

--- update_ad.sh
+++ update_ad.sh
@@ -5,8 +5,8 @@ git config --global user.email "aipromptgenius@gmail.com"
 git config --global user.name "Ben Finch"
 
 # Rest of the script
-echo 'Sponsored by <u><a href="https://imgcreator.zmo.ai/?ref=chatgptpromptgenius" target="_blank">ZMO.AI</a></u> - The Ultimate Free Generator for AI Anime, Photo, and Design. Text to image, image to image & ChatGPT powered AI designer.' > public/ads/current.txt
-echo 'https://imgcreator.zmo.ai/?ref=chatgptpromptgenius' > public/ads/currentUrl.txt
+echo 'Sponsored by <u><a href="https://aipromptpal.com/?ref=chatgptpromptgenius" target="_blank">aiPromptPal</a></u> - enhance ChatGPT by adding customizable right-click prompts, Gmail integration, and supplement search results.' > public/ads/current.txt
+echo 'https://aipromptpal.com/?ref=chatgptpromptgenius' > public/ads/currentUrl.txt
 git add public/ads/current.txt
 git add public/ads/currentUrl.txt
 git commit -m "Update ad"
