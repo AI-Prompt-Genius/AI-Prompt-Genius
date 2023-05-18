517223bb8794a0ebce5234b9f568211e34274464
benf2004 <12115686+benf2004@users.noreply.github.com>
2023-05-18 17:19:18 -0600

    build script I should've made a while ago

--- build.sh
+++ build.sh
@@ -13,3 +13,12 @@ cp -r src/* dist_mv2
 # Copy the manifest files to their respective dist directories
 cp manifests/mv3-manifest/manifest.json dist_mv3
 cp manifests/mv2-manifest/manifest.json dist_mv2
+
+zip -r -FS dist_mv3.zip dist_mv3
+
+cd dist_mv2
+zip -r -FS ../dist_mv2.zip *
+cd ..
+
+mv dist_mv3.zip ~/Downloads
+mv dist_mv2.zip ~/Downloads
