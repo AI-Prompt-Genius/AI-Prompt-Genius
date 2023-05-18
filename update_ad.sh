741b64b47306aa83c081a1ae2d48423d1b1c0f95
benf2004 <12115686+benf2004@users.noreply.github.com>
2023-05-18 13:57:00 -0600

    username

--- update_ad.sh
+++ update_ad.sh
@@ -2,7 +2,7 @@
 
 # Set Git user name and email
 git config --global user.email "aipromptgenius@gmail.com"
-git config --global user.name "Ben Finch"
+git config --global user.name "benf2004"
 
 # Rest of the script
 echo 'Sponsored by <u><a href="https://link.aipromptgenius.app/useai" target="_blank">UseChatGPT.AI</a></u> - Free ChatGPT Copilot on Chrome. Use ChatGPT (GPT-4 ✓) on any website without copy-pasting.' > public/ads/current.txt
