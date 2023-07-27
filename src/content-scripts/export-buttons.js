4aa6c398fbf7ff25bfccc1699de900504f1b3e0b
benf2004 <code@phooey.foo>
2023-07-27 14:08:15 -0600

    Localized exports

--- src/content-scripts/export-buttons.js
+++ src/content-scripts/export-buttons.js
@@ -3,9 +3,7 @@
 const ExportButtons = (function()
 {
 	const ADD_BUTTONS_DELAY = 500;
-	
-	//console.log(`Loading export buttons...`);
-	
+
 	let markdown_box = document.createElement("div");
 	markdown_box.setAttribute("id", "markdown_box");
 	if (document.querySelector("main")) document.querySelector('main').appendChild(markdown_box);
@@ -59,6 +57,39 @@ const ExportButtons = (function()
 		return childInnerHTML;
 	}
 
+	function convertChatToMarkdown(chat, title)
+	{
+		let string = "";
+		if(title)
+		{
+			string += "# " + title + "\n";
+		}
+		else
+		{
+			string += "# " + `${tr("chatgpt")} ${tr("conversation")}` + "\n";
+		}
+		string += "\n"; // two newlines because MD is like that
+		let convo = chat;
+		for(let i = 0; i < convo.length; i++)
+		{
+			let speaker = i % 2 === 0 ? tr("human") : tr("assistant");
+			string += "**" + speaker + ":**\n";
+			string += convo[i] + "\n";
+			string += "\n";
+			string += "***\n";
+			string += "\n";
+		}
+
+		// timestamp
+		let date = getDate();
+		let time = getTime();
+
+		string += `${tr("exported_on")} ` + date + " " + time + ".";
+
+		let blob = encodeStringAsBlob(string);
+		return blob;
+	}
+
 	
 	function getCurrentChatText()
 	{
@@ -66,7 +97,7 @@ const ExportButtons = (function()
 		// new version is probaby more robust, can't see how they would change the flex col anytime soon
 		let chatContainer = mainElement.querySelector(".flex-col");
 		// what is one part of a conversation called again? let's just call it a chat bubble
-		let chatBubbleElements = chatContainer.children;;
+		let chatBubbleElements = chatContainer.children;
 		let chat = [];
 
 		const isPlus = isPaidSubscriptionActive() ?? false;
@@ -130,8 +161,8 @@ const ExportButtons = (function()
 	
 	function createDropDown()
 	{
-		let button_class = 'flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm';
-		let shareSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" style="fill: white" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3V320c0 17.7 14.3 32 32 32s32-14.3 32-32V109.3l73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64c0 53 43 96 96 96H352c53 0 96-43 96-96V352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V352z"/></svg>`;
+		const button_class = 'flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm';
+		const shareSVG = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" width="16" style="fill: white"  viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>`;
 		 
 		let html =
 		`
@@ -140,9 +171,8 @@ const ExportButtons = (function()
 		<a id="download-pdf-button" class="${button_class}"><svg viewBox="0 0 24 24" width="18" height="19" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg> ${tr("download")} PDF</a>
 		<a id="download-png-button" class="${button_class}"><svg xmlns="http://www.w3.org/2000/svg" style="fill: white" stroke="currentColor" width="18" height="19" viewBox="0 0 512 512"><path d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM323.8 202.5c-4.5-6.6-11.9-10.5-19.8-10.5s-15.4 3.9-19.8 10.5l-87 127.6L170.7 297c-4.6-5.7-11.5-9-18.7-9s-14.2 3.3-18.7 9l-64 80c-5.8 7.2-6.9 17.1-2.9 25.4s12.4 13.6 21.6 13.6h96 32H424c8.9 0 17.1-4.9 21.2-12.8s3.6-17.4-1.4-24.7l-120-176zM112 192c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48z"></path></svg> ${tr("download")} PNG</a>
 		<a id="download-markdown-button" class="${button_class}"><svg xmlns="http://www.w3.org/2000/svg" width="18" style="fill: white" viewBox="0 0 640 512"><path d="M593.8 59.1H46.2C20.7 59.1 0 79.8 0 105.2v301.5c0 25.5 20.7 46.2 46.2 46.2h547.7c25.5 0 46.2-20.7 46.1-46.1V105.2c0-25.4-20.7-46.1-46.2-46.1zM338.5 360.6H277v-120l-61.5 76.9-61.5-76.9v120H92.3V151.4h61.5l61.5 76.9 61.5-76.9h61.5v209.2zm135.3 3.1L381.5 256H443V151.4h61.5V256H566z"></path></svg> ${tr("export")} md</a>
-		<a id="share-link-button" class="${button_class}"><svg xmlns="http://www.w3.org/2000/svg" style="fill: white" viewBox="0 0 640 512" width="18"><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></svg> ${tr("share")} ${tr("link")}</a>
 		</div>
-		<a id="shareExport" class="dropbtn ${button_class}">${shareSVG} ${tr("share")} & ${tr("export")}</a>
+		<a id="shareExport" class="dropbtn ${button_class}">${shareSVG} ${tr("export")}</a>
 		</div>
 		`
 		const nav = document.querySelector('nav')
@@ -180,7 +210,6 @@ const ExportButtons = (function()
 		let pdfButton = document.querySelector("#download-pdf-button");
 		let pngButton = document.querySelector('#download-png-button');
 		let markdownButton = document.querySelector('#download-markdown-button');
-		let shareButton =  document.querySelector('#share-link-button');
 
 		pdfButton.onclick = () => {
 			ExportButtons.downloadThread(Format.PDF);
@@ -190,9 +219,6 @@ const ExportButtons = (function()
 		}
 		markdownButton.onclick = () => {
 			ExportButtons.downloadThread(Format.MD);
-		}
-		shareButton.onclick = () => {
-			ExportButtons.sendRequest();
 		}
 	}
 	
@@ -349,89 +375,6 @@ const ExportButtons = (function()
 					console.warn(`ExportButtons.downloandThread: format "${format}" not recognized.`);
 					break;
 			}
-		},
-		
-		// sends a request to ShareGPT
-		sendRequest: async function() {
-			function conversationData() {
-				let threadContainer = document.querySelector("[class*='react-scroll-to-bottom']>[class*='react-scroll-to-bottom']>div");
-				if (threadContainer === null) {
-					threadContainer = document.querySelector("main > div > div > div");
-				}
-				var result = {
-					avatarUrl: getAvatarImage(),
-					items: [],
-				};
-				let i = 0;
-				for (const node of threadContainer.children) {
-					//console.log(i)
-					if (isPaidSubscriptionActive() && i === 0){
-						//console.log("paid")
-						i += 1
-						continue;
-					}
-					const markdownContent = node.querySelector(".markdown");
-
-					// tailwind class indicates human or gpt
-					if ([...node.classList].includes("dark:bg-gray-800")) {
-						result.items.push({
-							from: "human",
-							value: node.textContent,
-						});
-						// if text-center, it might be ChatGPT plus model name
-					} else if ([...node.classList].includes('text-center')) {
-						result.items.push({
-							from: "gpt",
-							value: node.textContent
-						});
-						// if it's a GPT response, it might contain code blocks
-					} else if ([...node.classList].includes("bg-gray-50")) {
-						result.items.push({
-							from: "gpt",
-							value: markdownContent.outerHTML,
-						});
-					}
-					i += 1
-				}
-
-				return result;
-			}
-
-			function getAvatarImage() {
-				// Create a canvas element
-				const canvas = document.createElement("canvas");
-
-				let image = document.querySelectorAll("img")[1];
-				//console.log(image)
-
-				if (image === undefined) {
-					image = document.querySelector("img");
-				}
-
-				// Set the canvas size to 30x30 pixels
-				canvas.width = 30;
-				canvas.height = 30;
-
-				// Draw the img onto the canvas
-				canvas.getContext("2d").drawImage(image, 0, 0);
-
-				// Convert the canvas to a base64 string as a JPEG image
-				const base64 = canvas.toDataURL("image/jpeg");
-
-				return base64;
-			}
-			let cd = conversationData();
-			//console.log(conversationData())
-			const res = await fetch("https://sharegpt.com/api/conversations", {
-				body: JSON.stringify(cd),
-				headers: {
-					"Content-Type": "application/json",
-				},
-				method: "POST",
-			});
-			const {id} = await res.json();
-			const url = `https://shareg.pt/${id}`; // short link to the ShareGPT post
-			window.open(url, "_blank");
 		},
 	}
 })();
