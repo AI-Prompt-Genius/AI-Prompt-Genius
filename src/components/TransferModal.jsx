49c0fdc17a3612ab6e4bf88f79cd8dceeb86e514
benf2004 <code@phooey.foo>
2023-11-15 16:20:34 -0700

    transfer modal updates

--- src/components/TransferModal.jsx
+++ src/components/TransferModal.jsx
@@ -3,11 +3,13 @@ import k from "../i18n/keys.js";
 import {useTranslation} from "react-i18next";
 import Logo from "./Logo.jsx";
 import LanguageSelect from "./LanguageSelect.jsx";
+import Head4 from "./Head4.jsx";
+import Head2 from "./Head2.jsx";
 
 function TransferModal(){
     const { t, i18n } = useTranslation();
     const [page, setPage] = useState(1)
-    const MAX_PAGE_NUM = 3
+    const MAX_PAGE_NUM = 4
 
 
     function nextPage(){
@@ -40,25 +42,27 @@ function TransferModal(){
                     }
                     {page === 2 &&
                         <div className="p-4">
-                            <h2 className="text-xl font-semibold text-gray-800 mb-4">What's new?</h2>
-                            <h4 className="text-lg font-medium text-gray-700 mt-4 mb-2">Categories are now folders</h4>
+                            <Head2>What's new?</Head2>
+                            <Head4>Categories are now folders</Head4>
                             <p className="text-gray-600 mb-4">You now get to pick your own categories - now called folders.</p>
-                            <h4 className="text-lg font-medium text-gray-700 mt-4 mb-2">Use AI Prompt Genius anywhere</h4>
-                            <p className="text-gray-600 mb-4">AI Prompt Genius is now self-contained.
-                                You can access it anywhere by clicking the
-                                extension icon or opening the sidebar (Chrome 116+). Work with any AI model of your choice.</p>
-                            <h4 className="text-lg font-medium text-gray-700 mt-4 mb-2">More robust import and cloud syncing features</h4>
+
+                            <Head4>Use AI Prompt Genius anywhere</Head4>
+                            <p className="text-gray-600 mb-4">AI Prompt Genius is now self-contained. You can access it anywhere by clicking the extension icon or opening the sidebar (Chrome 116+). Work with any AI model of your choice.</p>
+
+                            <Head4>More robust import and cloud syncing features</Head4>
                             <p>You can mass import prompts using a CSV template. Cloud syncing is now more reliable and prominent.</p>
-                            <h4 className="text-lg font-medium text-gray-700 mt-4 mb-2">New sleek UI</h4>
+
+                            <Head4>New sleek UI</Head4>
                             <p>The new UI is more cohesive and modern.</p>
-                            <h4 className="text-lg font-medium text-gray-700 mt-4 mb-2">History, themes, and chat export are being retired</h4>
-                            <p className={"text-gray-600 mb-4"}>You can download a copy of your data <span className={"link"}> JSON here.</span></p>
+
+                            <Head4>History, themes, and chat export are being retired</Head4>
+                            <p className={"text-gray-600 mb-4"}>You can download a copy of your data <span className={"link-primary link"}> JSON here.</span> You can <a className="link link-primary" target={"_blank"} href={""}> click here</a> to learn more about how to continue using these features.</p>
                         </div>
                     }
                     {page === 3 &&
                         <div className="p-4">
                             <div className={"flex align-middle"}>
-                                <h4 className="text-lg font-medium text-gray-700 mt-3 mb-3 mr-3">Import categories as</h4>
+                                <div className={"inline mr-3"}><Head4>Import categories as</Head4></div>
                                 <select className={"select select-bordered"}>
                                     <option selected>None</option>
                                     <option>Folder</option>
@@ -66,12 +70,21 @@ function TransferModal(){
                                 </select>
                             </div>
                             <p>If you want to continue using the legacy categories, you can choose to import them as tags or folders.</p>
+                        </div>
+                    }
+                    {page === 4 &&
+                        <div className="p-4">
+                            <div className="p-4">
+                                <Head2>Next Steps</Head2>
+                                <Head4></Head4> {/* Assuming you want to keep this empty Head4 here */}
+                            </div>
 
                         </div>
                     }
                     <div className={"absolute bottom-0 mb-2 p-3"}>
                         <button disabled={page === 1} className={"btn mr-3 disabled:hidden"} onClick={prevPage}>Back</button>
                         <button disabled={page === MAX_PAGE_NUM} className={"btn disabled:hidden"} onClick={nextPage}>Next</button>
+                        <button disabled={page !== MAX_PAGE_NUM} className={"btn disabled:hidden"}>Close</button>
                     </div>
                 </div>
             </div>
