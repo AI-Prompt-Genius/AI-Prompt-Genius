16b671878053f1bb5a3d9a7508201c0f9941ca61
benf2004 <code@phooey.foo>
2023-11-21 17:44:28 -0700

    Build script and screenshots

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Directory to be zipped
+PLUGIN_DIR=plugin
+
+# Destination directory
+DEST_DIR=~/Downloads
+
+# Name of the zip file
+ZIP_FILE=AI_Prompt-Genius_v4_0_0.zip
+
+# Zip the directory
+zip -r "$ZIP_FILE" $(basename "$PLUGIN_DIR")
+
+# Move the zip file to the destination directory
+mv "$ZIP_FILE" "$DEST_DIR"
