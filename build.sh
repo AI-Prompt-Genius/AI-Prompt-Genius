032d46da5b32e97c4dd3a4adb602fcbb423b1e74
benf2004 <code@phooey.foo>
2023-11-28 16:35:31 -0700

    Update build script

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ rm -rf dist_mv3 dist_mv2
 # Create the dist directories
 mkdir -p dist_mv3 dist_mv2
 
-# Copy the files from "src" to "dist_mv3" and "dist_mv2"
-cp -r src/* dist_mv3
-cp -r src/* dist_mv2
+# Copy the files from "plugin" to "dist_mv3" and "dist_mv2"
+cp -r plugin/* dist_mv3
+cp -r plugin/* dist_mv2
 
 # Copy the manifest files to their respective dist directories
 cp manifests/mv3-manifest/manifest.json dist_mv3
