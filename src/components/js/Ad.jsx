c076da2169cb198a53f8beb6adfc906427028a82
benf2004 <code@phooey.foo>
2024-01-02 14:19:19 -0700

    advertisement

--- src/components/js/Ad.jsx
+++ src/components/js/Ad.jsx
@@ -3,19 +3,6 @@ export default function Ad() {
 
     return (
         <>
-        {lang === "en" &&
-        <p className={"text-sm"}>
-            <a
-                className={"link link-primary"}
-                href={"https://link.aipromptgenius.app/survey"}
-                target={"_blank"}
-            >
-                Click here
-            </a>{" "}
-            to give your feedback about AI Prompt Genius for a chance to win a $50 Amazon.com gift
-            card
-        </p>
-        }
         </>
     )
 }
