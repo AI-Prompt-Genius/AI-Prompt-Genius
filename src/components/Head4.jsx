3a05d5ea1c3331126b46d1b41366d78a10443632
benf2004 <code@phooey.foo>
2024-01-02 17:10:38 -0700

    Fix colors on headers

--- src/components/Head4.jsx
+++ src/components/Head4.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Head4 = ({ children }) => {
-    return <h4 className="text-lg font-medium text-gray-700 mt-4 mb-2">{children}</h4>
+    return <h4 className="text-lg font-medium mt-4 mb-2">{children}</h4>
 }
 
 export default Head4
