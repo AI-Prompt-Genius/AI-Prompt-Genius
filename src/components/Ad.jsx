1f78c3f6a47bf6547487d856763d9d53d63038fd
benf2004 <code@phooey.foo>
2024-02-20 10:09:37 -0700

    Ad

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -1,7 +1,18 @@
 export default function Ad() {
-    const lang = localStorage.getItem("lng") ?? "en"
+    //const lang = localStorage.getItem("lng") ?? "en"
 
-    return (<></>)
+    return (
+        <p>
+            <a
+                className={"link link-primary"}
+                href={"https://link.aipromptgenius.app/max-ai-me-jan2"}
+                target={"_blank"}
+            >
+                Sponsored by MaxAI.me
+            </a>{" "}
+            - Use 1-click AI Anywhere
+        </p>
+    )
 }
 /* 
 <p>
