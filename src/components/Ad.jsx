f9a844ca3b5887a79c614867fb00570bf3b49195
benf2004 <code@phooey.foo>
2024-03-23 13:25:34 -0600

    Ad

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -3,14 +3,6 @@ export default function Ad() {
 
     return (
         <p>
-            <a
-                className={"link link-primary"}
-                href={"https://link.aipromptgenius.app/max-ai-me-jan2"}
-                target={"_blank"}
-            >
-                Sponsored by MaxAI.me
-            </a>{" "}
-            - Use 1-click AI Anywhere
         </p>
     )
 }
