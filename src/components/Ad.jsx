4494845c57db61e0cc0cb045fc430a048eaa13fa
“Zach <zacharyyork0@gmail.com>
2024-06-13 16:08:33 -0600

    no ad

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -4,8 +4,41 @@ import { getProStatus } from "./js/pro.js"
 export default function Ad() {
     const isPro = getProStatus()
 
-//     return (
-//         <>
+    return (
+        <>
+            {!isPro && (
+                <p className={"text-sm"}>
+                    <a
+                        className={"link link-primary"}
+                        href={
+                            "https://chromewebstore.google.com/detail/ai-prompt-genius/jjdnakkfjnnbbckhifcfchagnpofjffo/reviews"
+                        }
+                        target={"_blank"}
+                    >
+                        Enjoying the extension? Leave a five star review.
+                    </a>{" "}
+                </p>
+            )}
+        </>
+    )
+}
+/* 
+{!isPro && (
+                <p className={"text-sm"}>
+                    <a
+                        className={"link link-primary"}
+                        href={
+                            "https://chromewebstore.google.com/detail/ai-prompt-genius/jjdnakkfjnnbbckhifcfchagnpofjffo/reviews"
+                        }
+                        target={"_blank"}
+                    >
+                        Enjoying the extension? Leave a five star review.
+                    </a>{" "}
+                </p>
+            )}
+ */
+
+
 //             {!isPro && (
 //                 <p className={"text-sm"}>
 //                     <a
@@ -21,18 +54,3 @@ export default function Ad() {
 //         </>
 //     )
 // }
-
-{!isPro && (
-                <p className={"text-sm"}>
-                    <a
-                        className={"link link-primary"}
-                        href={
-                            "https://chromewebstore.google.com/detail/ai-prompt-genius/jjdnakkfjnnbbckhifcfchagnpofjffo/reviews"
-                        }
-                        target={"_blank"}
-                    >
-                        Enjoying the extension? Leave a five star review.
-                    </a>{" "}
-                </p>
-            )}
-    }
